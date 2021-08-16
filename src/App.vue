eb2db801ce5f771e108332a362201db7df328376
Ischerba32 <i.scherba32@gmail.com>
2021-08-16 14:57:00 +0300

    Add pagination and routing

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,12 @@
   <div id="app">
     <PosterBg :poster="posterBg"></PosterBg>
     <MoviesList :list="moviesList" @changePoster="onChangePoster"></MoviesList>
+    <MoviesPagination
+      :current-page="currentPage"
+      :per-page="moviesPerPage"
+      :total="moviesLength"
+      @pageChanged="onPageChanged"
+      ></MoviesPagination>
   </div>
 </template>
 
@@ -9,25 +15,41 @@
 import { mapActions, mapGetters } from 'vuex';
 import MoviesList from "./components/MoviesList.vue";
 import PosterBg from "./components/PosterBg.vue";
+import MoviesPagination from "./components/MoviesPagination.vue";
 
 export default {
   name: "App",
   components: {
    MoviesList,
    PosterBg,
+   MoviesPagination,
   },
   data: () => ({
     posterBg: "",
   }),
   computed: {
-    ...mapGetters('movies', ["moviesList"]),
+    ...mapGetters('movies', ["moviesList", "currentPage", "moviesPerPage", "moviesLength"]),
+  },
+  watch: {
+    "$route.query": {
+      handler: "onPageQueryChange",
+      immediate: true,
+      deep: true
+    }
   },
   methods: {
-    ...mapActions('movies', ["fetchMovies"]),
+    ...mapActions('movies', ["changeCurrentPage"]),
     onChangePoster(poster) {
       this.posterBg = poster;
     },
-  }
+    onPageQueryChange({page = 1}) {
+      this.changeCurrentPage(Number(page));
+    },
+    onPageChanged(page) {
+      // console.log(this.$router);
+      this.$router.push({query: {page}})
+    }
+  },
 };
 </script>
 
